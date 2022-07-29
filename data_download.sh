da9c2b42066ece6b73cf3164255a940aee729049
Emily Muller <emilymuller199@gmail.com>
2022-07-29 14:18:18 +0100

    precommit changes

--- data_download.sh
+++ data_download.sh
@@ -2,15 +2,15 @@
 #PBS -l select=1:ncpus=1:mem=1gb
 
 cd $PBS_O_WORKDIR
-cd emily/phd/recode-perceptions/input       # ENTER PATH to recode-perceptions from $PBS_O_WORKDIR
+cd emily/phd/recode-perceptions/input   # ENTER PATH to recode-perceptions from $PBS_O_WORKDIR
 
 # TRAINING IMAGES
 wget http://data.csail.mit.edu/places-private/places365/places365standard_easyformat.tar -O places365standard_easyformat.tar
 
 filename='keep.txt'
 while read line; do
-tar -xvf places365standard_easyformat.tar places365_standard/train/$line 
-tar -xvf places365standard_easyformat.tar places365_standard/val/$line 
+tar -xvf places365standard_easyformat.tar places365_standard/train/$line
+tar -xvf places365standard_easyformat.tar places365_standard/val/$line
 done < $filename
 
 rm places365standard_easyformat.tar
