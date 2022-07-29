7823758af11ca3800ccccb39adc4583dfa0f147f
Emily Muller <emilymuller199@gmail.com>
2022-07-29 14:12:06 +0100

    precommit changes

--- data_download.sh
+++ data_download.sh
@@ -5,7 +5,7 @@ cd $PBS_O_WORKDIR
 cd emily/phd/recode-perceptions/input       # ENTER PATH to recode-perceptions from $PBS_O_WORKDIR
 
 # TRAINING IMAGES
-# wget http://data.csail.mit.edu/places-private/places365/places365standard_easyformat.tar -O places365standard_easyformat.tar
+wget http://data.csail.mit.edu/places-private/places365/places365standard_easyformat.tar -O places365standard_easyformat.tar
 
 filename='keep.txt'
 while read line; do
